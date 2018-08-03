0f396d009063d6ac89120b6bc8694f7ff6e6fd48
ocean <ocean.tsai@104.com.tw>
2018-08-03 14:32:39 +0800

    1.0.1
    1.0.1

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-module.exports = {
-  cleanTask: require('./scripts/cleanTask.js'),
-  jsTask: require('./scripts/jsTask.js'),
-  vendorJsTask: require('./scripts/vendorJsTask.js'),
-  cssTask: require('./scripts/cssTask.js'),
-  imageTask: require('./scripts/imageTask.js'),
-  htmlTask: require('./scripts/htmlTask.js'),
-  hotReloadTask: require('./scripts/hotReloadTask.js'),
-  commandTask: require('./scripts/commandTask.js'),
-  createApp: require('./scripts/createAppTask.js')
+const pathFile = `${process.cwd()}/config/PATH.json`;
+const optionsFile = `${process.cwd()}/config/options.js`;
+const layoutFile = `${process.cwd()}/config/layout.js`;
+require('./scripts/createAppTask.js')();
+
+module.exports = () => {
+  try{
+    const PATH = require(pathFile);
+    const {
+      cleanOptions,
+      scssOptions,
+      serverOptions,
+      hotReloadOptions,
+      htmlOptions,
+      imgOptions,
+      svgOptions,
+      vendorOptions,
+      jsOptions
+    } = require(optionsFile);  
+    //
+    const layoutOptions = require(layoutFile);
+    //console.log(cleanOptions)
+    require('./scripts/cleanTask.js')(cleanOptions);
+    //javascript task
+    require('./scripts/jsTask.js')({ jsOptions, vendorOptions });
+    //vendor js task
+    require('./scripts/vendorJsTask.js')(vendorOptions);
+    //scss task
+    require('./scripts/cssTask.js')(scssOptions);
+    //image task
+    require('./scripts/imageTask.js')({ imgOptions, svgOptions });
+    //html task
+    require('./scripts/htmlTask.js')({ htmlOptions, layoutOptions }, PATH);
+    //hot reload task
+    require('./scripts/hotReloadTask.js')({ serverOptions, hotReloadOptions });
+    //command task
+    require('./scripts/commandTask.js')();
+  }catch(e){
+    console.log(e)
+    console.log("you can enter gulp create:f2eApp")
+  }
 };
