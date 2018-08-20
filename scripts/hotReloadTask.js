7f38dead2a56402319a9f45871f00a90bd10e20b
ocean <ocean.tsai@104.com.tw>
2018-08-20 16:39:40 +0800

    add stop

--- scripts/hotReloadTask.js
+++ scripts/hotReloadTask.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
-const { httpServer } = require('./commonJoinPoint.js');
+const { httpServer, connect } = require('./commonJoinPoint.js');
 
 module.exports = (options) => {
   const { serverOptions, hotReloadOptions: { jsWatch, scssWatch, htmlWatch } } = options;
 
   gulp.task('connect', () => {
     httpServer(serverOptions);
+  });
+
+  gulp.task('stop', () => {
+    connect.serverClose();
   });
 
   gulp.task('watch', () => {
